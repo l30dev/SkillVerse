19493d44cedd97ff75e66cc50cb2a054a94eab4e
l30dev <leoghvaladze3134@gmail.com>
2025-06-25 21:21:54 +0400

    Login application simply for starting

--- src/main/java/com/example/skillverse/LoginApp.java
+++ src/main/java/com/example/skillverse/LoginApp.java
@@ -8,9 +8,13 @@ public class LoginApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        LoginPage loginPage = new LoginPage(stage);
+        WelcomePage.show(stage); // Start from welcome screen
+    }
+
+    public static void show(Stage stage) {
+        LoginPage loginPage = new LoginPage(stage); // You already have this class
         Scene scene = new Scene(loginPage.getLayout(), 350, 400);
-        scene.getStylesheets().add((getClass().getResource("loginStyle.css")).toExternalForm());
+        scene.getStylesheets().add(LoginApp.class.getResource("loginStyle.css").toExternalForm());
         stage.setTitle("Login Page");
         stage.setScene(scene);
         stage.show();
